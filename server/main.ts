07c6fffd1737386ec988558a0c2c8e211416e369
alan <alan@fanruan.com>
2020-06-19 09:34:17 +0800

    fix: 修改端口号

--- server/main.ts
+++ server/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8106);
+  await app.listen(8101);
 }
 bootstrap();
